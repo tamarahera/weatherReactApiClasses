dd637aff969d923a2135cc7ea353b034fb3c9a07
tamara_hera <tamara.herashchenko@gmail.com>
2023-08-17 18:28:55 +0200

    add header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,13 @@ import './header.scss';
 function Header() {
   return (
     <header className="header">
-
+      <nav>
+        <ul className="header__list">
+          <li className="header__item"><a href="#" className="header__link header__link--active">Start page</a></li>
+          <li className="header__item"><a href="#" className="header__link">Select a city</a></li>
+          <li className="header__item"><a href="#" className="header__link">News & Weather</a></li>
+        </ul>
+      </nav>
     </header>
   );
 }
